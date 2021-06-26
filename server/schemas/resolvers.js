b1b9616d448bfc01146521bee1b9be3dffccb6c4
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-25 19:03:11 -0700

    add query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,18 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+
+                return userData;
+            }
+
+            throw new AuthenticationError('not logged in!');
+        }
+    }
+}
